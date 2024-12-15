3ecd70f5a436e6a90b36f24be83f873f0b708a0c
Hp <Hp@DESKTOP-0EGL18S>
2024-12-15 15:26:13 +0500

    Add project files.

---
+++ Controllers/siteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace website.Controllers
+{
+    public class siteController : Controller
+    {
+        // GET: site
+        public ActionResult HOME()
+        {
+            return View();
+
+        }
+        public ActionResult SINGUP()
+        {
+            return View();
+        }
+        public ActionResult CATEGORIES()
+        {
+            return View();
+        }
+        public ActionResult ABOUT()
+        {
+            return View();
+        }
+        public ActionResult CONTACT()
+        {
+            return View();
+        }
+    }
+}
